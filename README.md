00973c4f2acd8c8dfeb422044b0b07dfa870eddf
Lukas Fruntke <lukas@fruntke.tech>
2021-07-16 20:19:56 +0200

    Add static build

---
+++ README.md
@@ -0,0 +1,2 @@
+# hosted-iteration-02
+Not tested! USE WITH CAUTION!
---
+++ precache-manifest.03823c39063b1b714db795c49fd8b91f.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1537cdbd2b2009846df99d2bce3e1270",
+    "url": "/hosted-iteration-02/index.html"
+  },
+  {
+    "revision": "780a774a2d3bbb844e34",
+    "url": "/hosted-iteration-02/static/css/3.0de5d550.chunk.css"
+  },
+  {
+    "revision": "0516de9b9664e773ba72",
+    "url": "/hosted-iteration-02/static/css/7.d2e210e0.chunk.css"
+  },
+  {
+    "revision": "7c1a64da5d08b8dcae94",
+    "url": "/hosted-iteration-02/static/css/main.6a231c89.chunk.css"
+  },
+  {
+    "revision": "db7f4bbe924436ac8057",
+    "url": "/hosted-iteration-02/static/js/0.e5b091aa.chunk.js"
+  },
+  {
+    "revision": "780a774a2d3bbb844e34",
+    "url": "/hosted-iteration-02/static/js/3.34d2a058.chunk.js"
+  },
+  {
+    "revision": "33d7367ad7cbb014aed2",
+    "url": "/hosted-iteration-02/static/js/4.78a1e781.chunk.js"
+  },
+  {
+    "revision": "317dda7e6c3c04e6c6e5",
+    "url": "/hosted-iteration-02/static/js/5.c9d065d2.chunk.js"
+  },
+  {
+    "revision": "ebccb469b46f8298acd1",
+    "url": "/hosted-iteration-02/static/js/6.7d340c6c.chunk.js"
+  },
+  {
+    "revision": "0516de9b9664e773ba72",
+    "url": "/hosted-iteration-02/static/js/7.18f2f8c7.chunk.js"
+  },
+  {
+    "revision": "4c7f487a594ee50f0c1c",
+    "url": "/hosted-iteration-02/static/js/8.e1d64ec4.chunk.js"
+  },
+  {
+    "revision": "af6cc89a7ba9ae9b5fae",
+    "url": "/hosted-iteration-02/static/js/9.9fdc4c02.chunk.js"
+  },
+  {
+    "revision": "7c1a64da5d08b8dcae94",
+    "url": "/hosted-iteration-02/static/js/main.a1544ae7.chunk.js"
+  },
+  {
+    "revision": "1f69d0ae126cb677c613",
+    "url": "/hosted-iteration-02/static/js/runtime-main.f334d086.js"
+  },
+  {
+    "revision": "ed522db26ac92787b6f45658da827f7f",
+    "url": "/hosted-iteration-02/static/media/Architektur.ed522db2.png"
+  },
+  {
+    "revision": "5d7039ade4e0232e73167d1570f3cbc5",
+    "url": "/hosted-iteration-02/static/media/lukas.5d7039ad.jpg"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/hosted-iteration-02/static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/hosted-iteration-02/static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/hosted-iteration-02/static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/hosted-iteration-02/static/media/primeicons.b0f5d02f.eot"
+  },
+  {
+    "revision": "9df64cedba3e8143d50a923ea291b1d9",
+    "url": "/hosted-iteration-02/static/media/scale.9df64ced.jpeg"
+  }
+]);
